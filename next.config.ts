b41ddac471a205ee68462817a233ea07e1ee76dc
almao16 <almaodeveloper@gmail.com>
2026-04-01 21:12:32 -0400

    Cleaning up next.config to fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-ignore - Ignoramos el error de tipo solo para el deploy, pero mantenemos la funcionalidad local
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
-  /* Cualquier otra opción que tengas... */
+  /* Aquí puedes agregar otras opciones en el futuro si las necesitas */
 };
 
 export default nextConfig;
