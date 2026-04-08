188326087e8a2c249a8958b93ebb0680fcbe12e4
almao16 <almaodeveloper@gmail.com>
2026-04-08 01:16:49 -0400

    ajustees para movl error

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // No molesta mientras programas
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = {
-  // Aquí NO pongas 'output: export' para que tus APIs sigan funcionando en Vercel
+  // Esta línea le dice a Next.js que ignore el conflicto y use Webpack para la PWA
+  turbopack: {}, 
 };
 
 export default withPWA(nextConfig);
